00cb16023b414ae1bfc1376d154f3b8304e8fee4
linux_china <linux_china@hotmail.com>
2014-11-17 14:30:31 +0800

    application service annotation

---
+++ src/main/java/org/mvnsearch/ddd/application/annotations/ApplicationService.java
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+package org.mvnsearch.ddd.application.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * application service annotation
+ *
+ * @author linux_china
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ApplicationService {
+}
