23834203b66a5231237ad8c85a8c1e6dd29b902b
linux_china <linux_china@hotmail.com>
2014-11-17 14:14:43 +0800

    domain factory annotation

---
+++ src/main/java/org/mvnsearch/ddd/domain/annotations/DomainFactory.java
@@ -0,0 +1,18 @@
+package org.mvnsearch.ddd.domain.annotations;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * domain factory annotation
+ *
+ * @author linux_china
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface DomainFactory {
+
+}
