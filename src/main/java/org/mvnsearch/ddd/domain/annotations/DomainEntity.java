867d9542af8cfce6ef04dba2784b7d0717f0e3d1
linux_china <linux_china@hotmail.com>
2014-11-17 14:14:26 +0800

    domain entity annotation

---
+++ src/main/java/org/mvnsearch/ddd/domain/annotations/DomainEntity.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.ddd.domain.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * domain entity annotation
+ *
+ * @author linux_china
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface DomainEntity {
+
+}
