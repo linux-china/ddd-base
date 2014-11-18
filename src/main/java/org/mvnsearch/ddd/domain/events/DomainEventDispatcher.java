944ffc13645d28803efca47e2f6805913213eabd
linux_china <linux_china@hotmail.com>
2014-11-18 17:40:20 +0800

    domain event bus

---
+++ src/main/java/org/mvnsearch/ddd/domain/events/DomainEventDispatcher.java
@@ -0,0 +1,23 @@
+package org.mvnsearch.ddd.domain.events;
+
+/**
+ * domain event bus
+ *
+ * @author linux_china
+ */
+public interface DomainEventDispatcher {
+    /**
+     * publish domain event
+     *
+     * @param domainEvent domain event
+     */
+    void publish(BaseDomainEvent domainEvent);
+
+    /**
+     * register event handler
+     *
+     * @param handler event handler
+     */
+    public void registerEventHandler(EventHandler handler);
+
+}
