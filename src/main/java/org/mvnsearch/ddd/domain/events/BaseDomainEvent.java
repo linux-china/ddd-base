8877caa9326d78c6a56176a1e37cbdf06c79f2f0
linux_china <linux_china@hotmail.com>
2014-11-18 18:29:01 +0800

    add occuredAt

--- src/main/java/org/mvnsearch/ddd/domain/events/BaseDomainEvent.java
+++ src/main/java/org/mvnsearch/ddd/domain/events/BaseDomainEvent.java
@@ -4,6 +4,7 @@ import org.mvnsearch.ddd.domain.BaseEntity;
 import org.mvnsearch.ddd.domain.annotations.DomainEvent;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,13 +17,18 @@ import java.util.Map;
 public class BaseDomainEvent<T extends BaseEntity> implements Serializable {
     protected Map<String, Object> context = new HashMap<String, Object>();
     protected T source;
+    /**
+     * occured timestamp
+     */
+    private Date occuredAt;
 
     public BaseDomainEvent() {
-
+        this.occuredAt = new Date();
     }
 
     public BaseDomainEvent(T source) {
         this.source = source;
+        this.occuredAt = new Date();
     }
 
     public T getSource() {
@@ -31,6 +37,14 @@ public class BaseDomainEvent<T extends BaseEntity> implements Serializable {
 
     public void setSource(T source) {
         this.source = source;
+    }
+
+    public Date getOccuredAt() {
+        return occuredAt;
+    }
+
+    public void setOccuredAt(Date occuredAt) {
+        this.occuredAt = occuredAt;
     }
 
     public Object getAttribute(String name) {
