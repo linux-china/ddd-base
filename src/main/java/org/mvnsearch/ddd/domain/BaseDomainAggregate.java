6ae9e03af10c050d24223220fd3fa57172502b0d
linux_china <linux_china@hotmail.com>
2014-11-18 12:17:47 +0800

    base domain aggregate

--- src/main/java/org/mvnsearch/ddd/domain/BaseDomainAggregate.java
+++ src/main/java/org/mvnsearch/ddd/domain/BaseDomainAggregate.java
@@ -1,5 +1,8 @@
 package org.mvnsearch.ddd.domain;
 
+import org.mvnsearch.ddd.domain.annotations.DomainAggregate;
+import org.mvnsearch.ddd.domain.annotations.DomainAggregateRoot;
+
 import java.io.Serializable;
 
 /**
@@ -7,12 +10,14 @@ import java.io.Serializable;
  *
  * @author linux_china
  */
+@DomainAggregate
 public abstract class BaseDomainAggregate<K extends Serializable> implements BaseEntity<K> {
     /**
      * get aggregate root
      *
      * @return root object
      */
+    @DomainAggregateRoot
     public abstract K getRoot();
 
     public K getId() {
