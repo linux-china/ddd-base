dd698419991dacb850b5d6b07e51d01648ac6c96
linux_china <linux_china@hotmail.com>
2014-11-18 12:13:46 +0800

    adjust

--- src/main/java/org/mvnsearch/ddd/domain/BaseEntity.java
+++ src/main/java/org/mvnsearch/ddd/domain/BaseEntity.java
@@ -1,53 +1,18 @@
 package org.mvnsearch.ddd.domain;
 
 import java.io.Serializable;
-import java.util.Date;
 
 /**
- * base entity
+ * base entity for domain entity and aggregate
  *
  * @author linux_china
  */
-public interface BaseEntity<T extends Serializable> {
+public interface BaseEntity<K extends Serializable> {
     /**
      * get entity id
      *
      * @return entity id
      */
-    public T getId();
+    public K getId();
 
-    /**
-     * get created timestamp
-     *
-     * @return created timestamp
-     */
-    public Date getCreatedAt();
-
-    /**
-     * set created timestamp
-     *
-     * @param createdAt created timestamp
-     */
-    public void setCreatedAt(Date createdAt);
-
-    /**
-     * get last updated timestamp
-     *
-     * @return last updated timestamp
-     */
-    public Date getUpdatedAt();
-
-    /**
-     * set updated timestamp
-     *
-     * @param updatedAt updated timestamp
-     */
-    public void setUpdatedAt(Date updatedAt);
-
-    /**
-     * get entity status
-     *
-     * @return entity status
-     */
-    public Integer getStatus();
 }
