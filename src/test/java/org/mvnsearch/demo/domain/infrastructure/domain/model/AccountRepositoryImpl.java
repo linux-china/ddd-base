aa77a9f195769a5683d9997d219b4fbc9e9184b1
linux_china <linux_china@hotmail.com>
2016-12-07 09:29:39 +0800

    account repository implementation

---
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/model/AccountRepositoryImpl.java
@@ -0,0 +1,23 @@
+package org.mvnsearch.demo.domain.infrastructure.domain.model;
+
+import org.mvnsearch.demo.domain.model.Account;
+import org.mvnsearch.demo.domain.model.AccountRepository;
+
+/**
+ * account repository implementation
+ *
+ * @author linux_china
+ */
+public class AccountRepositoryImpl implements AccountRepository {
+    public void create(Account account) {
+
+    }
+
+    public void update(Account account) {
+
+    }
+
+    public Account findOne(long id) {
+        return null;
+    }
+}
