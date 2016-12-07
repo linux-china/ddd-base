cbc593c9190e54e66a5cdfcabaf06e75bb0c8248
linux_china <linux_china@hotmail.com>
2016-12-07 09:29:57 +0800

    account service implementation

---
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/service/AccountServiceImpl.java
@@ -0,0 +1,23 @@
+package org.mvnsearch.demo.domain.infrastructure.domain.service;
+
+import org.mvnsearch.demo.domain.model.Account;
+import org.mvnsearch.demo.domain.service.AccountService;
+
+/**
+ * account service implementation
+ *
+ * @author linux_china
+ */
+public class AccountServiceImpl implements AccountService {
+    public void create(Account account) {
+
+    }
+
+    public void updatePassword(Long accountId, String oldPassword, String newPassword) {
+
+    }
+
+    public void resetPassword(Long accountId) {
+
+    }
+}
