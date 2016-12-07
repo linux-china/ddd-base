6251a17d395634367719b8467a32e318ff683693
linux_china <linux_china@hotmail.com>
2016-12-07 09:16:58 +0800

    account event

---
+++ src/test/java/org/mvnsearch/demo/domain/event/AccountEvent.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.demo.domain.event;
+
+import org.mvnsearch.ddd.domain.events.BaseDomainEvent;
+import org.mvnsearch.demo.domain.model.Account;
+
+/**
+ * account event
+ *
+ * @author linux_china
+ */
+public class AccountEvent extends BaseDomainEvent<Account> {
+    public static String CREATED_TYPE = "created";
+    public static String BLOCKED_TYPE = "blocked";
+
+    public AccountEvent(String type, Account account) {
+        setType(type);
+        setPayload(account);
+    }
+}
