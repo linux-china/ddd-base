f8d1038524baf60867577ae005f970e283777df0
linux_china <linux_china@hotmail.com>
2016-12-07 09:30:12 +0800

    account sepecification implementation

---
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/specification/AccountSpecImpl.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.demo.domain.infrastructure.domain.specification;
+
+import org.mvnsearch.demo.domain.specification.AccountSpec;
+
+import java.util.Optional;
+
+/**
+ * account sepecification implementation
+ *
+ * @author linux_china
+ */
+public class AccountSpecImpl implements AccountSpec {
+
+    public Optional<Long> isEmailUnique(String email) {
+        return null;
+    }
+}
