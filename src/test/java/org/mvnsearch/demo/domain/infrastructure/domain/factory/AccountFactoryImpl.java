569bd8ff32b34bb148bd0af2fe65b3fa52164115
linux_china <linux_china@hotmail.com>
2016-12-07 09:29:22 +0800

    account factory implementation

---
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/factory/AccountFactoryImpl.java
@@ -0,0 +1,15 @@
+package org.mvnsearch.demo.domain.infrastructure.domain.factory;
+
+import org.mvnsearch.demo.domain.factory.AccountFactory;
+import org.mvnsearch.demo.domain.model.Account;
+
+/**
+ * account factory implementation
+ *
+ * @author linux_china
+ */
+public class AccountFactoryImpl implements AccountFactory {
+    public Account createFromWechat(String wechatXml) {
+        return null;
+    }
+}
