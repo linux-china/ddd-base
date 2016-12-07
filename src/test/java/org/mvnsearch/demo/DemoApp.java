6ffda20212b5aa5eb9fe3b26e2f5e6fc75ff0668
linux_china <linux_china@hotmail.com>
2016-12-07 09:17:38 +0800

    demo app entrance

---
+++ src/test/java/org/mvnsearch/demo/DemoApp.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.demo;
+
+/**
+ * demo app entrance
+ *
+ * @author linux_china
+ */
+public class DemoApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Demo!!!");
+    }
+}
