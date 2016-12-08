f723ac7f7aa01ae959eb583526b01d40ef36dd2a
linux_china <linux_china@hotmail.com>
2016-12-09 00:33:21 +0800

    rename

--- src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/service/AccountServiceImpl.java
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/service/AccountServiceImpl.java
@@ -1,16 +1,25 @@
 package org.mvnsearch.demo.domain.infrastructure.domain.service;
 
+import org.mvnsearch.demo.domain.event.AccountEvent;
 import org.mvnsearch.demo.domain.model.Account;
 import org.mvnsearch.demo.domain.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
 
 /**
  * account service implementation
  *
  * @author linux_china
  */
+@Service
 public class AccountServiceImpl implements AccountService {
-    public void create(Account account) {
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
 
+    public void create(Account account) {
+        //todo implement create logic
+        eventPublisher.publishEvent(new AccountEvent(AccountEvent.CREATED_TYPE, account));
     }
 
     public void updatePassword(Long accountId, String oldPassword, String newPassword) {
