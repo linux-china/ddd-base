cc9984f263a4f07ee39586b209987953cb82a836
linux_china <linux_china@hotmail.com>
2016-12-09 00:32:33 +0800

    account processor

---
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/event/AccountEventProcessor.java
@@ -0,0 +1,19 @@
+package org.mvnsearch.demo.domain.infrastructure.domain.event;
+
+import org.mvnsearch.demo.domain.event.AccountEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * account processor implementation
+ *
+ * @author linux_china
+ */
+@Component
+public class AccountEventProcessor  {
+
+    @EventListener(AccountEvent.class)
+    public void handleAccountCreate(AccountEvent accountEvent) {
+
+    }
+}
