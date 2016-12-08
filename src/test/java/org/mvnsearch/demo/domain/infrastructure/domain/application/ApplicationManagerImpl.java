e4a643bf830681a63bd0107de0296d2a5668d44c
linux_china <linux_china@hotmail.com>
2016-12-08 17:54:31 +0800

    application manager implementation

---
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/domain/application/ApplicationManagerImpl.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.demo.domain.infrastructure.domain.application;
+
+import org.mvnsearch.demo.domain.application.ApplicationManager;
+import org.mvnsearch.demo.domain.model.Account;
+
+/**
+ *  application manager implementation
+ *
+ * @author linux_china
+ */
+public class ApplicationManagerImpl implements ApplicationManager {
+
+    public Account findByEmail(String email) {
+        return null;
+    }
+}
