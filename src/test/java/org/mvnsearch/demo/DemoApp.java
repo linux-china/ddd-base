aa880a59aa3860daa6b7c8420738ddc9e99e56d3
linux_china <linux_china@hotmail.com>
2016-12-09 00:34:16 +0800

    change to spring boot application

--- src/test/java/org/mvnsearch/demo/DemoApp.java
+++ src/test/java/org/mvnsearch/demo/DemoApp.java
@@ -1,12 +1,16 @@
 package org.mvnsearch.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * demo app entrance
  *
  * @author linux_china
  */
+@SpringBootApplication
 public class DemoApp {
     public static void main(String[] args) {
-        System.out.println("Hello Demo!!!");
+        SpringApplication.run(DemoApp.class, args);
     }
 }
