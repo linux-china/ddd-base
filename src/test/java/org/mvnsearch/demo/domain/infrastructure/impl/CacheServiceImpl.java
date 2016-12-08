823c17cf3a01ab669e7e79316a04d8dbd72f452f
linux_china <linux_china@hotmail.com>
2016-12-08 17:45:21 +0800

    cache service implementation

---
+++ src/test/java/org/mvnsearch/demo/domain/infrastructure/impl/CacheServiceImpl.java
@@ -0,0 +1,29 @@
+package org.mvnsearch.demo.domain.infrastructure.impl;
+
+import org.mvnsearch.demo.domain.infrastructure.CacheService;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * cache service implementation
+ *
+ * @author linux_china
+ */
+public class CacheServiceImpl implements CacheService {
+    private Map<String, Serializable> store = new ConcurrentHashMap<>();
+
+    public void put(String key, Serializable object) {
+        store.put(key, object);
+    }
+
+    public Optional<Serializable> get(String key) {
+        if (store.containsKey(key)) {
+            return Optional.of(store.get(key));
+        } else {
+            return Optional.empty();
+        }
+    }
+}
