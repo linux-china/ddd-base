9d104dcdb98fbef301cd482e6b6d364d87447d80
linux_china <linux_china@hotmail.com>
2018-05-02 13:33:49 -0700

    domain event with Map data

---
+++ src/main/java/org/mvnsearch/ddd/domain/events/MapDomainEvent.java
@@ -0,0 +1,11 @@
+package org.mvnsearch.ddd.domain.events;
+
+import java.util.Map;
+
+/**
+ * domain event with Map data
+ *
+ * @author linux_china
+ */
+public class MapDomainEvent extends DomainEvent<Map<String, Object>> {
+}
