099a3f7271f757e01f4922b1468e38b3980c8e09
linux_china <linux_china@hotmail.com>
2018-05-02 10:35:49 -0700

    update to cloud event style

--- src/main/java/org/mvnsearch/ddd/domain/events/DomainEvent.java
+++ src/main/java/org/mvnsearch/ddd/domain/events/DomainEvent.java
@@ -1,85 +1,136 @@
 package org.mvnsearch.ddd.domain.events;
 
 
+import java.io.Serializable;
+import java.net.URI;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 /**
- * domain event: https://github.com/cncf/wg-serverless/tree/master/proposals/open-events
+ * domain event from CloudEvents: https://github.com/cloudevents/spec/blob/v0.1/spec.md
  *
  * @author linux_china
  */
-public class DomainEvent<T> {
+public class DomainEvent<T extends Serializable> {
     /**
-     * ID
+     * cloud events version
      */
-    private String id = UUID.randomUUID().toString();
+    private String cloudEventsVersion = "0.1";
     /**
-     * event type
+     * event ID
      */
-    private String type;
+    private String eventID = UUID.randomUUID().toString();
     /**
-     * event context
+     * event type: com.example.someevent
      */
-    private Map<String, Object> attributes;
+    private String eventType;
     /**
-     * source
+     * event type version:
      */
-    private transient Object source;
+    private String eventTypeVersion;
     /**
-     * data
+     * additional metadata
+     */
+    private Map<String, Serializable> extensions;
+    /**
+     * event producer
+     */
+    private URI source;
+    /**
+     * content type for data, such as text/plain, application/json
+     */
+    private String contentType;
+    /**
+     * The event payload
      */
     private T data;
     /**
-     * System time when the event happened
+     * A link to the schema that the data attribute adheres to
      */
-    private final long createdAt;
+    private URI schemaURL;
+    /**
+     * Timestamp of when the event happened
+     */
+    private Date eventTime;
 
     public DomainEvent() {
-        this.createdAt = System.currentTimeMillis();
+        this.eventTime = new Date();
     }
 
     public DomainEvent(T data) {
         this.data = data;
-        this.createdAt = System.currentTimeMillis();
+        this.eventTime = new Date();
     }
 
     public DomainEvent(String type, T data) {
-        this.type = type;
+        this.contentType = type;
         this.data = data;
-        this.createdAt = System.currentTimeMillis();
+        this.eventTime = new Date();
     }
 
-    public DomainEvent(String type, T data, Object source) {
-        this.type = type;
+    public DomainEvent(String type, T data, URI source) {
+        this.contentType = type;
         this.data = data;
         this.source = source;
-        this.createdAt = System.currentTimeMillis();
+        this.eventTime = new Date();
     }
 
-    public String getId() {
-        return id;
+    public String getCloudEventsVersion() {
+        return cloudEventsVersion;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setCloudEventsVersion(String cloudEventsVersion) {
+        this.cloudEventsVersion = cloudEventsVersion;
     }
 
-    public String getType() {
-        return type;
+    public String getEventID() {
+        return eventID;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
     }
 
-    public Object getSource() {
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getEventTypeVersion() {
+        return eventTypeVersion;
+    }
+
+    public void setEventTypeVersion(String eventTypeVersion) {
+        this.eventTypeVersion = eventTypeVersion;
+    }
+
+    public Map<String, Serializable> getExtensions() {
+        return extensions;
+    }
+
+    public void setExtensions(Map<String, Serializable> extensions) {
+        this.extensions = extensions;
+    }
+
+    public URI getSource() {
         return source;
     }
 
-    public void setSource(Object source) {
+    public void setSource(URI source) {
         this.source = source;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
     }
 
     public T getData() {
@@ -90,18 +141,30 @@ public class DomainEvent<T> {
         this.data = data;
     }
 
-    public long getCreatedAt() {
-        return createdAt;
+    public URI getSchemaURL() {
+        return schemaURL;
     }
 
-    public Object getAttribute(String name) {
-        return attributes == null ? null : attributes.get(name);
+    public void setSchemaURL(URI schemaURL) {
+        this.schemaURL = schemaURL;
     }
 
-    public void setAttribute(String name, Object value) {
-        if (attributes == null) {
-            attributes = new HashMap<>();
+    public Date getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(Date eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public Serializable getExtension(String name) {
+        return extensions == null ? null : extensions.get(name);
+    }
+
+    public void setExtension(String name, Serializable value) {
+        if (extensions == null) {
+            extensions = new HashMap<>();
         }
-        this.attributes.put(name, value);
+        this.extensions.put(name, value);
     }
 }
