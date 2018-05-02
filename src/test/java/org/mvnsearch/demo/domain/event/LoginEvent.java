2d16f133d63ff02980efc22fd20218778708b32e
linux_china <linux_china@hotmail.com>
2018-05-02 11:34:23 -0700

    remove naming

--- src/test/java/org/mvnsearch/demo/domain/event/LoginEvent.java
+++ src/test/java/org/mvnsearch/demo/domain/event/LoginEvent.java
@@ -1,6 +1,5 @@
 package org.mvnsearch.demo.domain.event;
 
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import org.mvnsearch.ddd.domain.events.DomainEvent;
 import org.mvnsearch.demo.domain.model.Account;
 
@@ -9,7 +8,6 @@ import org.mvnsearch.demo.domain.model.Account;
  *
  * @author linux_china
  */
-@JsonNaming
 public class LoginEvent extends DomainEvent<Account> {
 
     public LoginEvent(Account account, String ip) {
