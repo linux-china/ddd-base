527ee1ecb732719048d7a3358c55728eee53add1
linux_china <linux_china@hotmail.com>
2018-05-02 11:29:01 -0700

    domain event builder

---
+++ src/main/java/org/mvnsearch/ddd/domain/events/DomainEventBuilder.java
@@ -0,0 +1,86 @@
+package org.mvnsearch.ddd.domain.events;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * domain event builder
+ *
+ * @author linux_china
+ */
+public class DomainEventBuilder<T extends Serializable> {
+    /**
+     * cloud event
+     */
+    private DomainEvent<T> domainEvent;
+
+    public static <K extends Serializable> DomainEventBuilder<K> newInstance() {
+        DomainEventBuilder<K> builder = new DomainEventBuilder<K>();
+        builder.domainEvent = new DomainEvent<K>();
+        return builder;
+    }
+
+    public DomainEventBuilder<T> eventID(String eventId) {
+        domainEvent.setEventID(eventId);
+        return this;
+    }
+
+    public DomainEventBuilder<T> eventType(String eventType) {
+        domainEvent.setEventType(eventType);
+        return this;
+    }
+
+    public DomainEventBuilder<T> eventTypeVersion(String eventTypeVersion) {
+        domainEvent.setEventTypeVersion(eventTypeVersion);
+        return this;
+    }
+
+
+    public DomainEventBuilder<T> source(URI source) {
+        domainEvent.setSource(source);
+        return this;
+    }
+
+
+    public DomainEventBuilder<T> contentType(String contentType) {
+        domainEvent.setContentType(contentType);
+        return this;
+    }
+
+    public DomainEventBuilder<T> data(T data) {
+        domainEvent.setData(data);
+        return this;
+    }
+
+    public DomainEventBuilder<T> schemaURL(URI schemaURL) {
+        domainEvent.setSchemaURL(schemaURL);
+        return this;
+    }
+
+    public DomainEventBuilder<T> eventTime(Date eventTime) {
+        domainEvent.setEventTime(eventTime);
+        return this;
+    }
+
+    public DomainEventBuilder<T> extensions(Map<String, Serializable> extensions) {
+        domainEvent.setExtensions(extensions);
+        return this;
+    }
+
+    public DomainEventBuilder<T> extension(String name, Serializable value) {
+        domainEvent.setExtension(name, value);
+        return this;
+    }
+
+    public DomainEventBuilder<T> cloudEventsVersion(String cloudEventsVersion) {
+        domainEvent.setCloudEventsVersion(cloudEventsVersion);
+        return this;
+    }
+
+    public DomainEvent<T> build() {
+        return domainEvent;
+    }
+
+}
