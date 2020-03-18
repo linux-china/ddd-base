a813ce26dfeafcf47f8791c98c54eb6089bb2094
linux_china <libing.chen@gmail.com>
2020-03-17 18:06:49 -0700

    Order's LineItem

---
+++ src/test/java/org/mvnsearch/demo/domain/model/OrderLineItem.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.demo.domain.model;
+
+import org.mvnsearch.ddd.domain.annotations.DomainValueObject;
+
+/**
+ * Order's LineItem: Value Object
+ *
+ * @author linux_china
+ */
+@DomainValueObject
+public class OrderLineItem {
+}
