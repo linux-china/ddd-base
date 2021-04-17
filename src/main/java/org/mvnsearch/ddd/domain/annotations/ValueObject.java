54004f9675429f48706f46bf2a270148931d49a5
linux_china <libing.chen@gmail.com>
2021-04-16 20:40:29 -0700

    Renamed to ValueObject

--- src/main/java/org/mvnsearch/ddd/domain/annotations/ValueObject.java
+++ src/main/java/org/mvnsearch/ddd/domain/annotations/ValueObject.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface DomainValueObject {
+public @interface ValueObject {
 
 }
